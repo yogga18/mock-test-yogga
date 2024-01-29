f97ffdfaa08b73ba862ac95b4fb177bd99277905
Yogga Aditya Candra <yogga@ecampus.ut.ac.id>
2024-01-29 14:15:11 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import UserInfo from './pages/UserInfo/index.jsx';
+
+function App() {
+  return (
+    <>
+      <UserInfo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ButtonCustom/Button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <div className='button-wrapper-side-b'>
+      <button className='button-side-b'>Continue</button>
+    </div>
+  );
+};
+
+export default Button;
